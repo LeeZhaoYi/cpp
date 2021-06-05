2cab051e1d1754459bd21334ad56f9559bb51148
LeeZhaoYi <lzy290lzy@163.com>
2021-06-05 19:33:14 +0800

    代码

--- 二分图.cpp
+++ 二分图.cpp
@@ -16,15 +16,21 @@ viod dfs(int step,int c);
 	for(int i=1;i<=n;i++)
 	{
 		if(map[step][i]!=0)
-		{
 			dfs(i,-c);
-		}
 	}
 
-return 0;
+    return 0;
+
 }
+
 int main()
 {
+    vector <int> vect[20000]
+    {
+        for(int t=1;t<=n;t++)
+            if(color[t]==0)
+                dfs(t,1);
+    }
 	cin>>n>>m;
 	for(int i=1;i<m;i++)
 	{
@@ -33,11 +39,13 @@ int main()
 		map[x][y]=1;
 		map[y][x]=1;
 	}
+
 	for(int i=1;i<=n;i++)
 	{
 		c=-1;
 		dfs(i,c);
 	}
+
 	if(no==0)
 	{
 		for(int i=1;i<=n;i++)
@@ -47,7 +55,9 @@ int main()
 			cout<<i<<" ";
 		}
 	}
+
 	cout<<endl;
+
 	for(int i=1;i<=n;i++)
 	{
 		if(color[i]==-1)
@@ -59,7 +69,15 @@ int main()
 
     low=1;
     night=m;
-    invd=()
-    for(int j=1;j<=)
+    mid=(low+high);
+    for(int j=1;j<=mid;j++)
+    {
+        u=str[i].x;
+        v=str[i].y;
+        vect[u].push.back(v);
+        vect[v].push.back(u);
+    }
+
 	return 0;
-	}
+
+}
